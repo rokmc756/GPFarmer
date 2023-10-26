a067b1854513282f9f7bd73cfada369f60aa3e66
Jack Moon <rokmc756@gmail.com>
2023-10-26 22:06:19 +0900

    Updated for minio to support SSL and download binary

---
+++ roles/minio/files/README.md
@@ -0,0 +1,3 @@
+gzip minio -S -20231025.gz
+gunzip minio-20231025.gz
+mv minio-20231025 minio
--- scripts/force-uninstall-gpcc.sh
+++ scripts/force-uninstall-gpcc.sh
@@ -7,21 +7,26 @@
 MASTER_HOST="71"
 HOSTS_RANGE="71 75"
 
+#MASTER_HOST="81"
+#HOSTS_RANGE="81 85"
+
+ROOT_USER=root
+
 #
 MASTER_HOST_IP=192.168.0.$MASTER_HOST
 
-ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && source /usr/local/greenplum-cc/gpcc_path.sh && gpcc stop;"
-ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && source /usr/local/greenplum-cc/gpcc_path.sh && gppkg -r MetricsCollector;"
+#ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && source /usr/local/greenplum-cc/gpcc_path.sh && gpcc stop;"
+#ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && source /usr/local/greenplum-cc/gpcc_path.sh && gppkg -r MetricsCollector;"
 
 for i in $HOSTS_RANGE
 do
-    ssh gpadmin@192.168.0.$i "rm -rf /usr/local/greenplum-cc* /usr/local/greenplum-solr /home/gpadmin/.pgpass /home/gpadmin/certs"
+    ssh $ROOT_USER@192.168.0.$i "rm -rf /usr/local/greenplum-cc* /usr/local/greenplum-solr /home/gpadmin/.pgpass /home/gpadmin/certs"
+    ssh $ROOT_USER@192.168.0.$i "killall ccagent"
+    ssh $ROOT_USER@192.168.0.$i "killall gpccws"
 done
 
-
-ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && psql template1 -c 'DROP ROLE gpmon'"
-ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && psql template1 -c 'gpconfig -c gp_enable_gpperfmon -v off'"
-ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && gpstop -ra"
-ssh gpadmin@$MASTER_HOST_IP "rm -rf $MASTER_DATA_DIRECTORY/gpperfmon/data/* $MASTER_DATA_DIRECTORY/gpperfmon/logs/* /home/gpadmin/.pgpass"
-ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && dropdb gpperfmon"
-
+#ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && psql template1 -c 'DROP ROLE gpmon'"
+#ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && psql template1 -c 'gpconfig -c gp_enable_gpperfmon -v off'"
+#ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && gpstop -ra"
+ssh $ROOT_USER@$MASTER_HOST_IP "rm -rf $MASTER_DATA_DIRECTORY/gpperfmon/data/* $MASTER_DATA_DIRECTORY/gpperfmon/logs/* /home/gpadmin/.pgpass"
+#ssh gpadmin@$MASTER_HOST_IP "source /usr/local/greenplum-db/greenplum_path.sh && dropdb gpperfmon"
