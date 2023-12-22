de4c2a024dbe4b1aa560e83c79d495f6bd8a5f5e
Jack Moon <rokmc756@gmail.com>
2023-12-22 23:07:52 +0900

    Updates for GPCC and GPDB

--- scripts/force-uninstall-gpdb.sh
+++ scripts/force-uninstall-gpdb.sh
@@ -28,10 +28,10 @@ do
         rm -f /etc/sysctl.d/99-sysctl.conf
         systemctl stop cgconfig;
         systemctl disable cgconfig;
-        userdel gpadmin
-        rm -rf /home/gpadmin/.ssh;
-        rm -rf /home/gpadmin/*;
-        rm -rf /home/gpadmin;
+        # userdel gpadmin
+        # rm -rf /home/gpadmin/.ssh;
+        # rm -rf /home/gpadmin/*;
+        # rm -rf /home/gpadmin;
         rm -f /etc/cgconfig.conf;
         "
 done
